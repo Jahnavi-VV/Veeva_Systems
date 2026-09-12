a2624283f5ce68c955c4cda8296feeccd902834b
K.Jahnavi Veeraveni <jahnavivv20@gmail.com>
2026-09-12 21:50:37 +0530

    Add files via upload

---
+++ Coding Practice/September14.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class September14{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int year=sc.nextInt();
+int feb=28;
+if (year%400==0 || (year%4==0 && year!=100)){
+feb=29;
+}
+int days=31 + feb + 31 + 30 + 31 + 30 + 31 + 31+13;
+int rem=days%7;
+if(rem==0){
+System.out.println("Monday");}
+else if(rem==1){
+System.out.println("Tuesday");}
+else if(rem==2){
+System.out.println("Wednesday");}
+else if(rem==3){
+System.out.println("Thursday");}
+else if(rem==4){
+System.out.println("Friday");}
+else if(rem==5){
+System.out.println("Saturday");}
+else if(rem==6){
+System.out.println("Sunday");}
+}
+}
