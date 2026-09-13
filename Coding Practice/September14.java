b0e8ac715361d6aec73b82c6556ce685bb034ced
K.Jahnavi Veeraveni <jahnavivv20@gmail.com>
2026-09-13 22:43:24 +0530

    Fix leap year condition and formatting issues

--- Coding Practice/September14.java
+++ Coding Practice/September14.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 int year=sc.nextInt();
 int feb=28;
-if (year%400==0 || (year%4==0 && year!=100)){
+if (year%400==0 || (year%4==0 && year%100!=0)){
 feb=29;
 }
 int days=31 + feb + 31 + 30 + 31 + 30 + 31 + 31+13;
